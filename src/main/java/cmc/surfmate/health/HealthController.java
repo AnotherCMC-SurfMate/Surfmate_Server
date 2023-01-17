c33a45a10424c8ac23dfb42c3ff25f3be9415051
jemin <jemin03120111@gmail.com>
2023-01-18 00:06:57 +0900

    .

--- src/main/java/cmc/surfmate/health/HealthController.java
+++ src/main/java/cmc/surfmate/health/HealthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    @GetMapping("health")
+    @GetMapping
     public String health()
     {
         return "health";
