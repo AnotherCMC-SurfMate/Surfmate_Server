d0e71d792a9c9e31e0be322686992d26614bc960
jemin <jemin03120111@gmail.com>
2023-01-19 23:28:39 +0900

    test: health check 테스트 다양화

--- src/main/java/cmc/surfmate/health/HealthController.java
+++ src/main/java/cmc/surfmate/health/HealthController.java
@@ -5,6 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * HealthController.java
  *
@@ -16,7 +19,23 @@ public class HealthController {
     @GetMapping("/health")
     public ResponseEntity<CommonResponse> getHealth()
     {
-        return ResponseEntity.ok(new CommonResponse(200,"성공"));
+        HashMap<String,Object> map = new HashMap<>();
+        HashMap<String,String> map2 = new HashMap<>();
+        map2.put("inner","머식");
+        map.put("outer",map2);
+        return ResponseEntity.ok(new CommonResponse(200,"성공",map));
+    }
+
+    @GetMapping("/health2")
+    public ResponseEntity<CommonResponse> getHealth2()
+    {
+        HashMap<String,String> map = new HashMap<>();
+        HashMap<String,String> map2 = new HashMap<>();
+        map2.put("first","머식");
+        map.put("second","제민");
+        List<HashMap<String, String>> result = List.of(map, map2);
+
+        return ResponseEntity.ok(new CommonResponse(200,"성공",result));
     }
     @GetMapping("/")
     public String health()
